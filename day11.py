60ef535ac391bcf99d0fde0d55d09fd62d0a72fc
A Akshay Guptha <akshayguptha1216@gmail.com>
2026-06-01 18:54:52 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,7 @@
+from calcy import add, subtract, multiply, divide
+print("=== Simple Calculator ===")
+print(add (5, 3))        
+print(subtract(10, 4))
+print(multiply(6, 7))
+print(divide(15, 3))
+print(divide(10, 0))
