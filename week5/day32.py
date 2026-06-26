ae8f3971065e8ffa941184ca9a3850604998d977
A Akshay Guptha <akshayguptha1216@gmail.com>
2026-06-26 17:27:02 +0530

    Add files via upload

--- week5/day32.py
+++ week5/day32.py
@@ -1,41 +1,41 @@
-# # Day 32 — Decorators
+# Day 32 — Decorators
 
-# def my_decorator(func):
-#     def wrapper():
-#         print("Before the function runs")
-#         func()
-#         print("After the function runs")
-#     return wrapper
+def my_decorator(func):
+    def wrapper():
+        print("Before the function runs")
+        func()
+        print("After the function runs")
+    return wrapper
 
-# @my_decorator
-# def say_hello():
-#     print("Hello!")
+@my_decorator
+def say_hello():
+    print("Hello!")
 
-# say_hello()
-
-
-# import time
-
-# # Decorator
-# def timer(func):
-#     def wrapper(*args, **kwargs):
-#         start = time.time()          # Start timer
-#         result = func(*args, **kwargs)  # Call the original function
-#         end = time.time()            # End timer
-
-#         print(f"{func.__name__} took {end - start:.2f} seconds")
-#         return result
-
-#     return wrapper
+say_hello()
 
 
-# @timer
-# def slow_function():
-#     time.sleep(1)
-#     print("Function complete")
+import time
+
+# Decorator
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()          # Start timer
+        result = func(*args, **kwargs)  # Call the original function
+        end = time.time()            # End timer
+
+        print(f"{func.__name__} took {end - start:.2f} seconds")
+        return result
+
+    return wrapper
 
 
-# slow_function()
+@timer
+def slow_function():
+    time.sleep(1)
+    print("Function complete")
+
+
+slow_function()
 
 class FileManager:
     def __init__(self, filename, mode):
